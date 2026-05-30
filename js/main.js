3399ddc9e6c70d96762e542f94e58ff1b98400fd
Juan Zamora <jzamora11p@gmail.com>
2026-05-30 00:56:04 -0500

    Navbar y Hero al 90%

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// NAVBAR
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('main-header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
