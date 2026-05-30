d1d5eb040585495b08fc0db664a809a382df683d
Juan Zamora <jzamora11p@gmail.com>
2026-05-30 16:57:15 -0500

    Se fixean errores del Projects Section

--- js/main.js
+++ js/main.js
@@ -7,3 +7,34 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section[id]");
+    const navLinks = document.querySelectorAll(".list-navbar a");
+
+    const observerOptions = {
+        root: null,
+        rootMargin: "-30% 0px -60% 0px",
+        threshold: 0
+    };
+
+    const observerCallback = (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const currentId = entry.target.getAttribute("id");
+                
+                navLinks.forEach((link) => {
+                    if (link.getAttribute("href") === `#${currentId}`) {
+                        link.classList.add("active");
+                    } else {
+                        link.classList.remove("active");
+                    }
+                });
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    
+    sections.forEach((section) => observer.observe(section));
+});
